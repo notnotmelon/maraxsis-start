31a0d8aaac9c91598e5d3741092a16e9094c7946
Zachary Picco <zacharypicco@gmail.com>
2025-03-31 13:30:07 -0500

    compat: Fixed progression locks when combined with rocs hardcore vulcanus.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -3,3 +3,11 @@ local utils = require("__any-planet-start__.utils")
 utils.set_prerequisites("sp-spidertron-automation", {"processing-unit", "tungsten-steel", "electric-engine", "bulk-inserter", "fluid-handling"})
 utils.add_recipes("sp-spidertron-automation", {"maraxsis-diesel-submarine"})
 utils.set_packs("sp-spidertron-automation", {"automation-science-pack", "logistic-science-pack"}, 100, 30)
+
+if mods["Rocs-Hardcore-Demolishers"] then
+    local utils = require("__any-planet-start__.utils")
+    utils.add_recipes("foundry", {
+        "concrete-from-molten-iron",
+        "casting-steel",
+    })
+end
