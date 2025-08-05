d4f85a447dbdd692474f6f441ea490f9eef84e28
Zachary Picco <zacharypicco@gmail.com>
2025-08-05 01:13:21 -0500

    fix: Fixed medium asteroids appearing in orbit above maraxsis. Fixed big asteroids appearing in-route from vulcanus/fulgora to maraxsis.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,4 +1,5 @@
-local utils = require("__any-planet-start__.utils")
+local utils = require "__any-planet-start__.utils"
+local asteroid_util = require "__space-age__.prototypes.planet.asteroid-spawn-definitions"
 
 utils.set_prerequisites("sp-spidertron-automation", {"processing-unit", "tungsten-steel", "electric-engine", "bulk-inserter", "fluid-handling"})
 utils.add_recipes("sp-spidertron-automation", {"maraxsis-diesel-submarine"})
@@ -11,3 +12,7 @@ if mods["Rocs-Hardcore-Demolishers"] then
         "casting-steel",
     })
 end
+
+data.raw.planet.maraxsis.asteroid_spawn_definitions, data.raw.planet.nauvis.asteroid_spawn_definitions = data.raw.planet.nauvis.asteroid_spawn_definitions, data.raw.planet.maraxsis.asteroid_spawn_definitions
+data.raw["space-connection"]["vulcanus-maraxsis"].asteroid_spawn_definitions = asteroid_util.spawn_definitions(asteroid_util.nauvis_vulcanus)
+data.raw["space-connection"]["fulgora-maraxsis"].asteroid_spawn_definitions = asteroid_util.spawn_definitions(asteroid_util.nauvis_fulgora)
